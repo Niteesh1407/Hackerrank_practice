057819e837a098fba189bbcd396186d65ec1b47c
niteesh1407 <117834365+Niteesh1407@users.noreply.github.com>
2022-12-24 19:17:47 +0530

    Create Arrays Introduction.cpp

---
+++ Arrays Introduction.cpp
@@ -0,0 +1,22 @@
+//link: https://www.hackerrank.com/challenges/arrays-introduction/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+CODE:
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin>>n;
+    int a
+    for(int i=0;i<n;i++)
+    cin>>a[i];
+    for(int i=n-1;i>=0;i--)
+    cout<<a[i]<<" ";   
+    return 0;
+}

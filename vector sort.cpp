969c229a258c478de71660824ea4cee0947400fd
niteesh1407 <117834365+Niteesh1407@users.noreply.github.com>
2022-12-26 15:27:22 +0530

    Create vector sort.cpp

---
+++ vector sort.cpp
@@ -0,0 +1,27 @@
+//link: https://www.hackerrank.com/challenges/vector-sort/problem?isFullScreen=true
+
+CODE:
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    vector<int>v;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        int x;
+        cin>>x;
+        v.push_back(x);
+    }   
+    sort(v.begin(),v.end());
+    for(int i=0;i<v.size();i++)
+    cout<<v[i]<<" ";
+    return 0;
+}
